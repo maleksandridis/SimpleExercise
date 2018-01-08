6a289b89104a9fc47c8ea8a5856c5bb3adafb642
maleksandridis <32519853+maleksandridis@users.noreply.github.com>
2018-01-08 02:25:12 +0200

    Update Project4.java

--- src/main/java/org/javelas/Javelas/Project4.java
+++ src/main/java/org/javelas/Javelas/Project4.java
@@ -15,7 +15,6 @@ public class Project4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
          Runnable runnable = new Runnable() {
             @Override
             public void run() {
