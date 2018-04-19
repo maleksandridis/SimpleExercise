ff2ff5fa509bc14e87d3f3a2ead56d0fb2461ff3
maleksandridis <32519853+maleksandridis@users.noreply.github.com>
2018-04-19 13:36:08 +0300

    Delete scanner

--- src/main/java/org/javelas/Javelas/SpellingTest.java
+++ src/main/java/org/javelas/Javelas/SpellingTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.util.Scanner;
 
 public class SpellingTest {
 
